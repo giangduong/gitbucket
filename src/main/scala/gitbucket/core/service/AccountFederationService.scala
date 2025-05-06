56608df25f2f09880ec42b8ada324188e861f3a0
tungbuivn <tungbuivn@gmail.com>
2025-05-06 17:00:59 +0700

    update

--- src/main/scala/gitbucket/core/service/AccountFederationService.scala
+++ src/main/scala/gitbucket/core/service/AccountFederationService.scala
@@ -27,20 +27,34 @@ trait AccountFederationService {
     mailAddress: String,
     preferredUserName: Option[String],
     fullName: Option[String]
-  )(implicit s: Session): Option[Account] =
+  )(implicit s: Session): Option[Account] = {
+
     getAccountByFederation(issuer, subject) match {
       case Some(account) if !account.isRemoved =>
         Some(account)
+
       case Some(account) =>
         logger.info(s"Federated user found but disabled: userName=${account.userName}, isRemoved=${account.isRemoved}")
         None
+
       case None =>
-        findAvailableUserName(preferredUserName, mailAddress) flatMap { userName =>
-          createAccount(userName, "[DUMMY]", fullName.getOrElse(userName), mailAddress, isAdmin = false, None, None)
-          createAccountFederation(issuer, subject, userName)
-          getAccountByUserName(userName)
+        val userName = preferredUserName.orElse(mailAddress.split("@").headOption).getOrElse("user")
+
+        getAccountByUserName(userName) match {
+          case Some(_) =>
+            // Username exists, only create federation mapping
+            createAccountFederation(issuer, subject, userName)
+            getAccountByUserName(userName)
+
+          case None =>
+            // Username does not exist, create user then federation
+            createAccount(userName, "[DUMMY]", fullName.getOrElse(userName), mailAddress, isAdmin = false, None, None)
+            createAccountFederation(issuer, subject, userName)
+            getAccountByUserName(userName)
         }
+
     }
+  }
 
   private def extractSafeStringForUserName(s: String) = """^[a-zA-Z0-9][a-zA-Z0-9\-_.]*""".r.findPrefixOf(s)
 
@@ -83,8 +97,16 @@ trait AccountFederationService {
   def hasAccountFederation(userName: String)(implicit s: Session): Boolean =
     AccountFederations.filter(_.userName === userName.bind).exists.run
 
-  def createAccountFederation(issuer: String, subject: String, userName: String)(implicit s: Session): Unit =
-    AccountFederations insert AccountFederation(issuer, subject, userName)
+  def createAccountFederation(issuer: String, subject: String, userName: String)(implicit s: Session): Unit = {
+    val exists = AccountFederations
+      .filter(af => af.issuer === issuer && af.subject === subject && af.userName === userName)
+      .exists
+      .run
+
+    if (!exists) {
+      AccountFederations insert AccountFederation(issuer, subject, userName)
+    }
+  }
 }
 
 object AccountFederationService extends AccountFederationService with AccountService
