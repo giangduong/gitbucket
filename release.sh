bb31f2ee2d2a160b876ebcc528cbd619e734328b
tungbuivn <tungbuivn@gmail.com>
2025-05-07 12:32:42 +0700

    fix release

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 RLVERSION=$(date '+%Y-%m-%d_%H-%M-%S')
 ver="v4.38.4.${RLVERSION}"
-gh repo set-default origin
+gh repo set-default https://github.com/giangduong/gitbucket
 gh release create $ver ./target/scala-2.13/gitbucket_2.13-4.38.4.war \
   --title "Release $ver" \
   --notes "This is the changelog or description of the release."
