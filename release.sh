d3e0aee51f45f8d894aba42fcfe9647a53bcbff5
tungbuivn <tungbuivn@gmail.com>
2025-05-07 15:31:31 +0700

    fix source commit

--- release.sh
+++ release.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
 
+set -e
+
+TARGET_BRANCH="v4.38.4"
 RELEASE_VERSION=$(date '+%Y%m%d_%H-%M-%S')
 TAG_VERSION="v4.38.4.${RELEASE_VERSION}"
+WAR_FILE="./target/scala-2.13/gitbucket_2.13-4.38.4.war"
 
+# Check prerequisites
+
+if [ ! -f "$WAR_FILE" ]; then
+  echo "WAR file not found at $WAR_FILE"
+  exit 1
+fi
+
+# Set default repo
 gh repo set-default https://github.com/giangduong/gitbucket
 
-gh release create "$TAG_VERSION" ./target/scala-2.13/gitbucket_2.13-4.38.4.war \
+# Fetch the latest commit hash from the target branch
+COMMIT_SHA=$(git rev-parse origin/$TARGET_BRANCH)
+echo $COMMIT_SHA
+exit
+# Create a tag pointing to that commit
+git tag "$TAG_VERSION" "$COMMIT_SHA"
+git push origin "$TAG_VERSION"
+
+# Create GitHub release
+gh release create "$TAG_VERSION" "$WAR_FILE" \
   --title "Release $TAG_VERSION" \
   --notes "This is the changelog or description of the release."
-
