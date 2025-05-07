e9cbea81860f505e377f54c70f7018d448da5ede
tungbuivn <tungbuivn@gmail.com>
2025-05-07 15:16:37 +0700

    fix release script

--- release.sh
+++ release.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-RLVERSION=$(date '+%Y%m%d_%H-%M-%S')
-ver="v4.38.4.${RLVERSION}"
+RELEASE_VERSION=$(date '+%Y%m%d_%H-%M-%S')
+TAG_VERSION="v4.38.4.${RELEASE_VERSION}"
 
 gh repo set-default https://github.com/giangduong/gitbucket
 
-gh release create $ver ./target/scala-2.13/gitbucket_2.13-4.38.4.war \
-  --title "Release $ver" \
+gh release create "$TAG_VERSION" ./target/scala-2.13/gitbucket_2.13-4.38.4.war \
+  --title "Release $TAG_VERSION" \
   --notes "This is the changelog or description of the release."
 
